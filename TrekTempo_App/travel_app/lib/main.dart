f43835774ff89f8258063c9d21254894154c2de9
Naami Ahmed <naamiahmed27@gmail.com>
2024-07-21 11:03:15 +0530

    Basic Flutter source code Added (+100 files)
    Basic Flutter source code Added (+100 files)

---
+++ TrekTempo_App/travel_app/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TrekTempo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color.fromARGB(
+            255, 66, 96, 110), // Set background color to light blue
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'TrekTempo',
+                style: TextStyle(
+                  color: Colors.white, // Change fontcolor to color
+                  fontSize: 64,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Find your dream Destination with us',
+                style: TextStyle(
+                  color: Colors.white, // Change fontcolor to color
+                  fontSize: 24,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              SizedBox(height: 250),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your button action here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  fixedSize: Size(200, 50),
+                ),
+                child: Text(
+                  'GET STARTED',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
