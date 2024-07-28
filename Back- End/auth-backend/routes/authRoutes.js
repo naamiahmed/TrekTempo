b51efaca4296f0b22baa97ce3f8c2fcc18690931
Naami Ahmed <naamiahmed27@gmail.com>
2024-07-28 22:42:00 +0530

    chore: Add new npm packages and update existing ones (for Back End)

---
+++ Back- End/auth-backend/routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { signup, login, forgotPassword, resetPassword } = require('../controllers/authController');
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+
+module.exports = router;
