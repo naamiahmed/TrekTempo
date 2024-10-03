6bf2d26ac15a5b7cc3081562e5a7fcab2384031a
Naami Ahmed <naamiahmed27@gmail.com>
2024-10-03 08:23:02 +0530

    Refactor trip plan details page to use custom widget structure

--- Front- End/lib/Pages/HomePage_Featurs/TripPlanning/TripDetails.dart
+++ Front- End/lib/Pages/HomePage_Featurs/TripPlanning/TripDetails.dart
@@ -50,11 +50,16 @@ class _TripPlanDetailsPageState extends State<TripPlanDetailsPage> {
           Text('Duration: ${tripDetails!['days']} days'),
           SizedBox(height: 20),
 
-          // Displaying places for each day
-          for (var i = 0; i < tripDetails!['places'].length; i++) ...[
-            Text('Day ${i+1} - Places to Visit', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            SizedBox(height: 10),
-            Text(tripDetails!['places'][i], style: TextStyle(fontSize: 16)),
+          // Displaying trip plan for each day using the custom widget structure
+          for (var i = 0; i < tripDetails!['days']; i++) ...[
+            TripDayWidget(
+              day: i + 1,
+              dayPlan: {
+                'morning': tripDetails!['places'][i]['morning'],
+                'evening': tripDetails!['places'][i]['evening'],
+                'night': tripDetails!['places'][i]['night']
+              },
+            ),
             SizedBox(height: 20),
           ],
 
@@ -73,6 +78,109 @@ class _TripPlanDetailsPageState extends State<TripPlanDetailsPage> {
           ]
         ],
       ),
+    );
+  }
+}
+
+class TripDayWidget extends StatelessWidget {
+  final int day;
+  final Map<String, dynamic> dayPlan;
+
+  TripDayWidget({required this.day, required this.dayPlan});
+
+  @override
+  Widget build(BuildContext context) {
+    var headline6 = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+    );
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Day $day',
+              style: Theme.of(context).textTheme.bodyLarge, // Use project theme
+            ),
+            Divider(),
+            buildTimeSection('Morning', dayPlan['morning']),
+            Divider(),
+            buildTimeSection('Evening', dayPlan['evening']),
+            Divider(),
+            buildTimeSection('Night', dayPlan['night']),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTimeSection(String timeOfDay, List<dynamic> places) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          timeOfDay,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 8),
+        ...places.map((place) => buildPlaceWidget(place)).toList(),
+      ],
+    );
+  }
+
+  Widget buildPlaceWidget(Map<String, dynamic> place) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.network(
+          place['imageUrl'], // URL of place image
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    place['name'],
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 5),
+                  Icon(
+                    Icons.location_on,
+                    size: 14,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    place['location'],
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+              SizedBox(height: 4),
+              Text(place['description']),
+              SizedBox(height: 4),
+              Text(
+                'Weather: ${place['weather']}',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
