4a13667def4edd31a4f09eb91efcbf6d17038420
Mohamed Faleel Mohamed Rifthan Fathir <rifthanfathir33@gmail.com>
2024-10-07 23:08:08 +0530

    Refactor place.js and tripPlaceController.js: Add getAllPlaces endpoint and remove unused imports and commented code

--- Back- End/controllers/tripPlaceController.js
+++ Back- End/controllers/tripPlaceController.js
@@ -1,38 +1,41 @@
-const TripPlace = require('../models/TripPlace'); // Adjust the path according to your project structure
+const TripPlace = require("../models/TripPlace"); // Adjust the path according to your project structure
 
 const createTripPlace = async (req, res) => {
   try {
-      const tripPlacesData = req.body; 
-      const tripPlaces = await TripPlace.insertMany(tripPlacesData);
-      res.send({ success: true, tripPlaces });
+    const tripPlacesData = req.body;
+    const tripPlaces = await TripPlace.insertMany(tripPlacesData);
+    res.send({ success: true, tripPlaces });
   } catch (error) {
-      res.send({ success: false, message: error.message }); 
+    res.send({ success: false, message: error.message });
   }
 };
 
 const getTripPlaces = async (req, res) => {
-    try {
-        const district = req.params.district;
+  try {
+    const district = req.params.district;
 
-        // Check if district is provided in the URL parameters
-        if (district) {
-            const tripPlaces = await TripPlace.find({ district: district });
-            if (tripPlaces.length === 0) {
-                return res.send({ success: false, message: `No places found in ${district}.` });
-            }
-            return res.send({ success: true, places: tripPlaces });
-        }
-
-        // If no district is provided, return all places
-        const tripPlaces = await TripPlace.find();
-        if (tripPlaces.length === 0) {
-            return res.send({ success: false, message: 'No places found.' });
-        }
-
-        res.send({ success: true, places: tripPlaces });
-    } catch (error) {
-        res.send({ success: false, message: error.message });
+    // Check if district is provided in the URL parameters
+    if (district) {
+      const tripPlaces = await TripPlace.find({ district: district });
+      if (tripPlaces.length === 0) {
+        return res.send({
+          success: false,
+          message: `No places found in ${district}.`,
+        });
+      }
+      return res.send({ success: true, places: tripPlaces });
     }
+
+    // If no district is provided, return all places
+    const tripPlaces = await TripPlace.find();
+    if (tripPlaces.length === 0) {
+      return res.send({ success: false, message: "No places found." });
+    }
+
+    res.send({ success: true, places: tripPlaces });
+  } catch (error) {
+    res.send({ success: false, message: error.message });
+  }
 };
 
 module.exports = { getTripPlaces, createTripPlace };
