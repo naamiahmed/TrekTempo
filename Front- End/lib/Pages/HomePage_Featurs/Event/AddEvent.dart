562208fc657ce8b212bdece9e018e34f24235898
Rashidha <139774901+rashidhamnf@users.noreply.github.com>
2024-10-07 17:32:45 +0530

    Update AddEvent.dart
    add more

--- Front- End/lib/Pages/HomePage_Featurs/Event/AddEvent.dart
+++ Front- End/lib/Pages/HomePage_Featurs/Event/AddEvent.dart
@@ -22,6 +22,44 @@ class _AddEventPageState extends State<AddEventPage> {
   final _formKey = GlobalKey<FormState>();
   String? _selectedDistrict;
 
+
+ DateTime? _selectedDate;        // For one-day events
+  DateTimeRange? _dateRange;      // For multi-day events
+  String _eventType = 'One Day';  // Default event type is 'One Day'
+
+  // Function to select a single date for one-day events
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+
+  // Function to select a date range for multi-day events
+  Future<void> _selectDateRange(BuildContext context) async {
+    DateTimeRange? newDateRange = await showDateRangePicker(
+      context: context,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+
+    if (newDateRange != null) {
+      setState(() {
+        _dateRange = newDateRange;
+      });
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,10 +69,6 @@ class _AddEventPageState extends State<AddEventPage> {
           icon: Icon(Icons.arrow_back),
           onPressed: () {
             // Add back navigation functionality
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => EventPage()),
-            );
           },
         ),
         backgroundColor: Colors.white,
@@ -46,6 +80,15 @@ class _AddEventPageState extends State<AddEventPage> {
           child: ListView(
             children: [
               TextFormField(
+
+                      validator: (value)
+                      {
+                          if(value!.isEmpty)
+                          return "Event Name can't be empty";
+                          //Event Name is must
+                          return null;
+                      },
+
                 decoration: InputDecoration(
                   labelText: 'Event Name',
                   prefixIcon: Icon(Icons.edit),
@@ -60,13 +103,20 @@ class _AddEventPageState extends State<AddEventPage> {
                     _selectedDistrict = newValue;
                   });
                 },
+                 validator: (value)
+                      {
+                          if(value!.isEmpty)
+                          return "District can't be empty";
+                          //Event Name is must
+                          return null;
+                      },
                 decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.location_on),
+                  prefixIcon: Icon(Icons.public),
                   labelText: 'District',
                   border: OutlineInputBorder(),
                   
                 ),
-                items: <String>['District 1', 'District 2', 'District 3']
+                items: <String>['Colombo', 'Kandy', 'Jaffna','Batticlo', 'puttalam', 'Trincomale']
                     .map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
@@ -76,24 +126,110 @@ class _AddEventPageState extends State<AddEventPage> {
               ),
               SizedBox(height: 16),
               TextFormField(
+
+                validator: (value)
+                      {
+                          if(value!.isEmpty)
+                          return "Event Location can't be empty";
+                          //Event Name is must
+                          return null;
+                      },
                 decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.location_on),
                   labelText: 'Event Location',
                   border: OutlineInputBorder(),
                 ),
               ),
               SizedBox(height: 16),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Date',
-                  prefixIcon: Icon(Icons.calendar_today),
-                  border: OutlineInputBorder(),
-                ),
-                onTap: () async {
-                  // Set date field with the picked date
-                },
+
+
+
+             Text('Event Type', style: TextStyle(fontWeight: FontWeight.bold)),
+              Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile<String>(
+                      title: Text('One Day'),
+                      value: 'One Day',
+                      groupValue: _eventType,
+                      onChanged: (value) {
+                        setState(() {
+                          _eventType = value!;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile<String>(
+                      title: Text('More Than One Day'),
+                      value: 'More Than One Day',
+                      groupValue: _eventType,
+                      onChanged: (value) {
+                        setState(() {
+                          _eventType = value!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
+
+              // Conditional date input based on selected event type
+              if (_eventType == 'One Day')
+                GestureDetector(
+                  onTap: () => _selectDate(context), // Open single date picker
+                  child: AbsorbPointer(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Event Date',
+                        prefixIcon: Icon(Icons.calendar_today),
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: TextEditingController(
+                        text: _selectedDate == null
+                            ? 'Select Date'
+                            : '${_selectedDate!.toLocal()}'.split(' ')[0],
+                      ),
+                    ),
+                  ),
+                ),
+              
+              if (_eventType == 'More Than One Day')
+                GestureDetector(
+                  onTap: () => _selectDateRange(context), // Open date range picker
+                  child: AbsorbPointer(
+                    child: TextFormField(
+
+                      validator: (value)
+                      {
+                          if(value!.isEmpty)
+                          return "Event Dates can't be empty";
+                          //Event Name is must
+                          return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Event Dates',
+                        prefixIcon: Icon(Icons.calendar_today),
+                        border: OutlineInputBorder(),
+                      ),
+                      controller: TextEditingController(
+                        text: _dateRange == null
+                            ? 'Select Date Range'
+                            : '${_dateRange!.start.toLocal()} - ${_dateRange!.end.toLocal()}',
+                      ),
+                    ),
+                  ),
+                ),
               SizedBox(height: 16),
               TextFormField(
+ validator: (value)
+                      {
+                          if(value!.isEmpty)
+                          return "Event Description can't be empty";
+                          //Event Name is must
+                          return null;
+                      },
+                
                 decoration: InputDecoration(
                   labelText: 'Event Description',
                   border: OutlineInputBorder(),
