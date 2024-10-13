49a66b71c630714851cb44399b4a7e50b001838a
Naami Ahmed <naamiahmed27@gmail.com>
2024-10-13 17:36:07 +0530

    Refactor adminEventController.js and adminEvent.js

--- Back- End/routes/adminEvent.js
+++ Back- End/routes/adminEvent.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllEvents } = require('../controllers/adminEventController');
+const { getAllEvents, deleteEvent } = require('../controllers/adminEventController');
 
 router.get("/getAllEvents", getAllEvents);
-
-
+router.delete("/deleteEvent/:id", deleteEvent);
 
 module.exports = router;
