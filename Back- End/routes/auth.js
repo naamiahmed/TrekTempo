97eaa93b09738f39d7dcbdd525adbb008489c65f
Mohamed Faleel Mohamed Rifthan Fathir <rifthanfathir33@gmail.com>
2024-10-14 13:29:38 +0530

    as

--- Back- End/routes/auth.js
+++ Back- End/routes/auth.js
@@ -1,57 +1,14 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
 // Sign Up
-router.post('/signup', async (req, res) => {
-    const { name, email, password } = req.body;
-
-    try {
-        // Check if user already exists
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({ msg: 'User already exists' });
-        }
-
-        // Create new user
-        user = new User({
-            name,
-            email,
-            password: await bcrypt.hash(password, 10), // Hash password
-        });
-
-        await user.save();
-        res.status(201).json({ msg: 'User registered successfully' });
-    } catch (err) {
-        res.status(500).json({ msg: 'Server error' });
-    }
-});
+router.post('/signup', userController.signUp);
 
 // Sign In
-router.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
+router.post('/signin', userController.signIn);
 
-    try {
-        // Check for user
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ msg: 'Invalid credentials' });
-        }
-
-        // Compare passwords
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ msg: 'Invalid credentials' });
-        }
-
-        // Generate JWT
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token });
-    } catch (err) {
-        res.status(500).json({ msg: 'Server error' });
-    }
-});
+// Update User Details
+router.put('/update', userController.updateUser);
 
 module.exports = router;
