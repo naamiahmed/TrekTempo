43d5f36b3bf9329f6aa012e9204f69357e3d4018
Naami Ahmed <naamiahmed27@gmail.com>
2024-11-04 00:51:37 +0530

    Add file upload functionality for accommodation and accepted events

--- Back- End/routes/adminAcceptedEvent.js
+++ Back- End/routes/adminAcceptedEvent.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllAcceptedEvents, deleteAcceptedEvent , moveEventToAccepted } = require('../controllers/adminAcceptedEventController');
+const { getAllAcceptedEvents, deleteAcceptedEvent, moveEventToAccepted, createAcceptedEvent, upload } = require('../controllers/adminAcceptedEventController');
 
 router.get("/getAllAcceptedEvents", getAllAcceptedEvents);
 router.delete("/deleteAcceptedEvent/:id", deleteAcceptedEvent);
 router.post("/moveEventToAccepted/:id", moveEventToAccepted);
+router.post("/createAcceptedEvent", upload.single('image'), createAcceptedEvent);
 
 module.exports = router;
