5f3322d28b71ca5e289e3ce7d15207fefd61dd98
Naami Ahmed <naamiahmed27@gmail.com>
2024-11-04 10:56:54 +0530

    Add getAllAccommodations endpoint to retrieve all accommodations

--- Back- End/routes/accommodation.js
+++ Back- End/routes/accommodation.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createAccommodation, getAccommodation, getAccommodationByDistrictandBudget, upload } = require('../controllers/accommodationController');
+const { createAccommodation, getAccommodation, getAccommodationByDistrictandBudget, getAllAccommodations, upload } = require('../controllers/accommodationController');
 
 router.post("/createAccommodation", upload.single('image'), createAccommodation);
 router.post("/getAccommodation", getAccommodation);
 router.get("/getAccommodation/:district?/:budget?", getAccommodationByDistrictandBudget);
+router.get("/getAllAccommodations", getAllAccommodations);
 
 module.exports = router;
